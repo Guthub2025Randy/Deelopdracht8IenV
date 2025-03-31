d8c4af71d07a1d39d335151250e79ea3e87577aa
Guthub2025Randy <randydexdekker@gmail.com>
2025-03-31 13:58:38 +0200

    Recursieve functie gefixxed

--- Randy/recursief_traagheidsmoment_en_plaatdikte.py
+++ Randy/recursief_traagheidsmoment_en_plaatdikte.py
@@ -11,33 +11,33 @@ import numpy as np
 import matplotlib.pyplot as plt
 # a is nodige sterkte, b is berekende sterkte
 
-def traagheid_en_plaatdikte_schalen(benodigde, t, traagheidsmoment, D, B):
-    plaatdiktes = [0.001]
-    traagheidsmomenten = [traagheidsmoment]
+def traagheid_en_plaatdikte_schalen(benodigde, t, traagheidsmoment, D, B, plaatdikteses, traagheidsmomentenen):
     if traagheidsmoment<=benodigde:
         t += 0.001 # in m
-        plaatdiktes.append(t)
+        plaatdikteses.append(t)
         traagheidsmoment2 = (t*D**2*(D-B))/6
-        traagheidsmomenten.append(traagheidsmoment2)
-        c, d = traagheid_en_plaatdikte_schalen(benodigde, t, traagheidsmoment2, D, B)
+        traagheidsmomentenen.append(traagheidsmoment2)
+        return traagheid_en_plaatdikte_schalen(benodigde, t, traagheidsmoment2, D, B, plaatdikteses, traagheidsmomentenen)
     elif traagheidsmoment>=benodigde:
-        return t, traagheidsmoment
+        plt.figure(figsize=(8,5))
+        return t, traagheidsmoment, plaatdikteses, traagheidsmomentenen
     elif Error == True:
         print("Error binnen recursieve functie")
         return None
-    plt.figure(figsize=(8,5))
-    plt.plot(traagheidsmomenten, plaatdiktes, color='b', label='Traagheidsmomenten')
-    plt.xlabel("Traagheidsmomenten (T) in [m4]")
-    plt.ylabel("Plaatdiktes (t) in [m]")
-    plt.title("Traagheidsmomenten als functie van de plaatdiktes")
-    plt.legend()
-    plt.grid(True)
-    return traagheid_en_plaatdikte_schalen(benodigde, c, d, D, B)
 
 tplaat = 0.001
 benodigde1 = 100
 Dikte = 10
 Breedte = 6 
 traagheidsmoment1 = (tplaat*Dikte**2*(Dikte-Breedte))/6
+plaatdiktes = [0.001]
+traagheidsmomenten = [traagheidsmoment1]
 
-traagheid_en_plaatdikte_schalen(benodigde1, tplaat, traagheidsmoment1, Dikte, Breedte)
+treal, Traagheidsreal, plaatdiktesreal, traagheidsmomentenreal = traagheid_en_plaatdikte_schalen(benodigde1, tplaat, traagheidsmoment1, Dikte, Breedte, plaatdiktes, traagheidsmomenten)
+
+plt.plot(traagheidsmomentenreal, plaatdiktesreal, color='b', label='Traagheidsmomenten')
+plt.xlabel("Traagheidsmomenten (T) in [m4]")
+plt.ylabel("Plaatdiktes (t) in [m]")
+plt.title("Traagheidsmomenten als functie van de plaatdiktes")
+plt.legend()
+plt.grid(True)
